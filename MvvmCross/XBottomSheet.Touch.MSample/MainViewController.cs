6d15563c40c542a5e8bc93a845ecd7548081138f
Alex Stefan <alex.steefan@gmail.com>
2019-01-11 19:39:38 +0200

    Add XBottomSheet control

--- MvvmCross/XBottomSheet.Touch.MSample/MainViewController.cs
+++ MvvmCross/XBottomSheet.Touch.MSample/MainViewController.cs
@@ -1,14 +1,40 @@
-﻿using MvvmCross.Platforms.Ios.Presenters.Attributes;
+﻿using CoreGraphics;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using MvvmCross.Platforms.Ios.Views;
+using UIKit;
 using XBottomSheet.Core.MSample.ViewModels;
+using XBottomSheet.Touch.Views;
 
 namespace XBottomSheet.Touch.MSample
 {
     [MvxRootPresentation]
     public partial class MainViewController : MvxViewController<MainViewModel>
     {
+        BottomSheetViewController bottomSheetViewController;
+
         public MainViewController() : base("MainViewController", null)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SetupBottomSheet();
+        }
+
+        private void SetupBottomSheet()
+        {
+            // Create BottomSheetViewController
+            var bottom = UIScreen.MainScreen.Bounds.Height - UIApplication.SharedApplication.StatusBarFrame.Height;
+            bottomSheetViewController = new BottomSheetViewController(100, 300, bottom, true, BottomSheetState.Bottom);
+
+            // Add BottomSheetViewController as a child view 
+            AddChildViewController(bottomSheetViewController);
+            View.AddSubview(bottomSheetViewController.View);
+            bottomSheetViewController.DidMoveToParentViewController(this);
+
+            // BottomSheetViewController frame
+            bottomSheetViewController.View.Frame = new CGRect(0, View.Frame.GetMaxY(), View.Frame.Width, View.Frame.Height);
         }
     }
 }
