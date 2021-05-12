b1dae00747177d3e02e31a53c62fecde74e1b0e8
Alexandru.Stefan <alex.steefan@gmail.com>
2021-05-12 22:43:44 +0200

    Fix sample project build

--- XBottomSheet.Samples/XBottomSheet.Touch/ViewController.designer.cs
+++ XBottomSheet.Samples/XBottomSheet.Touch/ViewController.designer.cs
@@ -1,28 +1,35 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace XBottomSheet.Touch.Sample
 {
-    [Register ("ViewController")]
-    partial class ViewController
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton btMain { get; set; }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton btMain { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (btMain != null) {
-                btMain.Dispose ();
-                btMain = null;
-            }
-        }
-    }
+		[Outlet]
+		UIKit.UIButton btTest { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btMain != null) {
+				btMain.Dispose ();
+				btMain = null;
+			}
+
+			if (btTest != null) {
+				btTest.Dispose ();
+				btTest = null;
+			}
+		}
+	}
 }
